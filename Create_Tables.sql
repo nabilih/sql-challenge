a377fdd4c92b5ede60121fd2b87f151f2f346505
nabilih <58374027+nabilih@users.noreply.github.com>
2020-04-03 20:21:56 -0400

    Add files via upload

---
+++ Create_Tables.sql
@@ -0,0 +1,82 @@
+﻿CREATE TABLE "departments" (
+    "dept_no" varchar(50)   NOT NULL,
+    "dept_name" varchar(255)   NOT NULL,
+    CONSTRAINT "pk_departments" PRIMARY KEY (
+        "dept_no"
+     )
+);
+
+CREATE TABLE "employees" (
+    "emp_no" varchar(50)   NOT NULL,
+    "birth_date" date   NOT NULL,
+    "first_name" varchar(255)   NOT NULL,
+    "last_name" varchar(255)   NOT NULL,
+    "gender" char(1)   NOT NULL,
+    "hire_date" date   NOT NULL,
+    CONSTRAINT "pk_employees" PRIMARY KEY (
+        "emp_no"
+     )
+);
+
+CREATE TABLE "dept_emp" (
+    "dept_emp_id"  SERIAL  NOT NULL,
+    "emp_no" varchar(50)   NOT NULL,
+    "dept_no" varchar(50)   NOT NULL,
+    "from_date" date   NOT NULL,
+    "to_date" date  DEFAULT '9999-01-01',
+    CONSTRAINT "pk_dept_emp" PRIMARY KEY (
+        "dept_emp_id"
+     )
+);
+
+CREATE TABLE "titles" (
+    "title_id"  SERIAL  NOT NULL,
+    "emp_no" varchar(50)   NOT NULL,
+    "title" varchar(100)   NOT NULL,
+    "from_date" date   NOT NULL,
+    "to_date" date  DEFAULT '9999-01-01',
+    CONSTRAINT "pk_titles" PRIMARY KEY (
+        "title_id"
+     )
+);
+
+CREATE TABLE "dept_manager" (
+    "dept_manager_id"  SERIAL  NOT NULL,
+    "dept_no" varchar(50)   NOT NULL,
+    "emp_no" varchar(50)   NOT NULL,
+    "from_date" date   NOT NULL,
+    "to_date" date  DEFAULT '9999-01-01',
+    CONSTRAINT "pk_dept_manager" PRIMARY KEY (
+        "dept_manager_id"
+     )
+);
+
+CREATE TABLE "salaries" (
+    "salaries_id"  SERIAL  NOT NULL,
+    "emp_no" varchar(50)   NOT NULL,
+    "salary" int   NOT NULL,
+    "from_date" date   NOT NULL,
+    "to_date" date  DEFAULT '9999-01-01',
+    CONSTRAINT "pk_salaries" PRIMARY KEY (
+        "salaries_id"
+     )
+);
+
+ALTER TABLE "dept_emp" ADD CONSTRAINT "fk_dept_emp_emp_no" FOREIGN KEY("emp_no")
+REFERENCES "employees" ("emp_no");
+
+ALTER TABLE "dept_emp" ADD CONSTRAINT "fk_dept_emp_dept_no" FOREIGN KEY("dept_no")
+REFERENCES "departments" ("dept_no");
+
+ALTER TABLE "titles" ADD CONSTRAINT "fk_titles_emp_no" FOREIGN KEY("emp_no")
+REFERENCES "employees" ("emp_no");
+
+ALTER TABLE "dept_manager" ADD CONSTRAINT "fk_dept_manager_dept_no" FOREIGN KEY("dept_no")
+REFERENCES "departments" ("dept_no");
+
+ALTER TABLE "dept_manager" ADD CONSTRAINT "fk_dept_manager_emp_no" FOREIGN KEY("emp_no")
+REFERENCES "employees" ("emp_no");
+
+ALTER TABLE "salaries" ADD CONSTRAINT "fk_salaries_emp_no" FOREIGN KEY("emp_no")
+REFERENCES "employees" ("emp_no");
+
